81e99339c81e7dff15ab7157c36bca1d7142890f
lghiggino <lghiggino@gmail.com>
2021-08-25 22:20:05 -0300

    new tests

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -4,12 +4,20 @@ const app = require("../app")
 
 const api = supertest(app)
 
-test.only("notes are returned as json", async () => {
-    await api
-        .get("/api/notes")
-        .expect(200)
-        .expect("Content-Type", /application\/json/)
+describe("READ tests", () => {
+    it("notes are returned as json", async () => {
+        await api
+            .get("/api/notes")
+            .expect(200)
+            .expect("Content-Type", /application\/json/)
+    })
+
+    it("should return one note by the id", async () => {
+        const res = await api.get("/api/notes/1")
+        expect(res.id).toBe(1)
+    })
 })
+
 
 afterAll(() => {
     mongoose.connection.close()
