8d29c3a0d33908af610fdc830fb17a7f8f033003
lghiggino <lghiggino@gmail.com>
2021-08-26 20:58:02 -0300

    superApp redo

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -4,20 +4,31 @@ const app = require("../app")
 
 const api = supertest(app)
 
-describe("READ tests", () => {
-    it("notes are returned as json", async () => {
-        await api
-            .get("/api/notes")
-            .expect(200)
-            .expect("Content-Type", /application\/json/)
-    })
+// describe("READ tests", () => {
+//     it("notes are returned as json", async () => {
+//         await api
+//             .get("/api/notes")
+//             .expect(200)
+//             .expect("Content-Type", /application\/json/)
+//     })
 
-    it("should return one note by the id", async () => {
-        const res = await api.get("/api/notes/1")
-        expect(res.id).toBe(1)
-    })
+//     it("should return one note by the id", async () => {
+//         const res = await api.get("/api/notes/1")
+//         expect(res.id).toBe(1)
+//     })
+// })
+
+
+// afterAll(() => {
+//     mongoose.connection.close()
+// })
+
+test("notes are returned as json", async () => {
+    await api
+        .get("/api/notes")
+        .expect(200)
+        .expect("Content-Type", /application\/json/)
 })
-
 
 afterAll(() => {
     mongoose.connection.close()
