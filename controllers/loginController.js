1300099d014a5296a9cba9ce00d8e1b6e445b704
lghiggino <lghiggino@gmail.com>
2021-09-22 08:30:23 -0300

    loginRouter first commit

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,1 +1,28 @@
-//começar aqui https://fullstackopen.com/en/part4/token_authentication
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const loginRouter = require("express").Router()
+const User = require("../models/userModel")
+
+loginRouter.post("/", async (request, response) => {
+    const body = request.body
+
+    const user = await User.findOne({ username: body.username })
+    const passwordCorrect = (user === null) ? false : await bcrypt.compare(body.password, user.passwordHash)
+
+    if (!(user && passwordCorrect)) {
+        return response.status(401).json({
+            error: "invalid username or password"
+        })
+    }
+
+    const userForToken = {
+        username: user.username,
+        id: user._id
+    }
+
+    const token = jwt.sign(userForToken, process.env.SECRET)
+
+    response.status(200).setDefaultEncoding({ token, username: user.username, name: user.name })
+})
+
+module.exports = loginRouter
