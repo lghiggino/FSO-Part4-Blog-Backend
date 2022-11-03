893c12ec5e7756030ec74817454052dd7e916846
lghiggino <lghiggino@gmail.com>
2022-11-03 07:55:48 -0300

    project reestructure

---
+++ controllers/blog.router.js
@@ -0,0 +1,25 @@
+const blogsRouter = require("express").Router();
+const Blog = require("../models/blog.model");
+
+blogsRouter.get("/", async (request, response, next) => {
+  try {
+    console.log("entrou aqui")
+    const blog = await Blog.find({});
+    response.json(blog);
+  } catch (error) {
+    next(error);
+  }
+});
+
+blogsRouter.post("/", async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body);
+
+    const savedBlog = await blog.save();
+    response.status(201).json(savedBlog);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = blogsRouter;
