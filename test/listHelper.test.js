0e36ee60525db1eef710fe338a12f27d40ef7d2c
lghiggino <lghiggino@gmail.com>
2022-11-04 07:37:14 -0300

    4.4

--- test/listHelper.test.js
+++ test/listHelper.test.js
@@ -1,8 +1,50 @@
 const listHelper = require("../utils/list_helper");
 
-test("dummy returns one", () => {
-  const blogs = [];
+describe("total likes", () => {
+  const listWithZeroBlog = [];
 
-  const result = listHelper.dummy(blogs);
-  expect(result).toBe(1);
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithZeroBlog);
+    expect(result).toBe(0);
+  });
+
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  const listWithTwoBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b23a676234d17f8",
+      title: "idl ",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 3,
+      __v: 0,
+    },
+  ];
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithTwoBlog);
+    expect(result).toBe(8);
+  });
 });
