12cae408689e1765e46dfb48d0a4b504a2422bef
lghiggino <lghiggino@gmail.com>
2022-11-20 16:52:18 -0300

    4.18

--- controllers/blog.router.js
+++ controllers/blog.router.js
@@ -1,6 +1,7 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog.model");
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
 blogsRouter.get("/", async (request, response, next) => {
   try {
@@ -11,17 +12,38 @@ blogsRouter.get("/", async (request, response, next) => {
   }
 });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 blogsRouter.post("/", async (request, response, next) => {
   try {
     const body = request.body;
 
+    const token = getTokenFrom(request);
+    const decodedToken = jwt.verify(token, process.env.SECRET);
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: "token missing or invalid" });
+    }
+
     const user = await User.findById(body.userId);
 
-    const blog = new Blog(request.body);
+    const blog = new Blog({
+      title: request.body.title,
+      author: request.body.author,
+      url: request.body.url,
+      likes: request.body.likes,
+      user: user._id,
+    });
 
     const savedBlog = await blog.save();
     user.blogs = user.blogs.concat(savedBlog._id);
     await user.save();
+
     response.status(201).json(savedBlog);
   } catch (error) {
     next(error);
